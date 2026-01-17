c057abc90cd21feff1bae55bba6dbf7975129ba5
mikolaj <mikolaj.gawor@protonmail.com>
2026-01-17 18:28:58 +0100

    Add theme colors and test button

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color redDark = Color(0xFFC62E2E);
+  static const Color redLight = Color(0xFFF95454);
+
+  static const Color blueDark = Color(0xFF0D92F4);
+  static const Color blueLight = Color(0xFF77CDFF);
+
+  static const Color gray = Color(0xFF9E9E9E);
+
+
+  
+
+}
